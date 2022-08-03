f1f02084a940b895dd68b604d7bdc3f89738fc95
MK-Sohan <mksohan786@gmail.com>
2022-08-03 21:15:40 +0600

    k

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // const { application } = require("express");
-
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,29 +11,38 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbikj86.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect()
-        const allServiceCollection = client.db("project-eventy-data-collection").collection("all-service");
-        const allReviewCollection = client.db("project-eventy-data-collection").collection("all-review");
+  try {
+    await client.connect();
+    const allServiceCollection = client
+      .db("project-eventy-data-collection")
+      .collection("all-service");
+    const allReviewCollection = client
+      .db("project-eventy-data-collection")
+      .collection("all-review");
+    const allEventListCollection = client
+      .db("project-eventy-data-collection")
+      .collection("allEvent-List");
 
-
-        app.post('/post-review', async (req, res) => {
-            const postReview = await allReviewCollection.insertOne(req.body)
-            res.send(postReview)
-          })
-
-    } finally {
-    }
+    app.post("/post-review", async (req, res) => {
+      const postReview = await allReviewCollection.insertOne(req.body);
+      res.send(postReview);
+    });
+  } finally {
   }
-  run().catch(console.dir);
-  
-  app.get("/", (req, res) => {
-    res.send("Eventy server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log("Listning to port", port);
-  });
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Eventy server is running");
+});
+
+app.listen(port, () => {
+  console.log("Listning to port", port);
+});
