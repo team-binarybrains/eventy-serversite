8ef44df7ee56dd403279c19a78c30af96cdbb33e
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-08-09 20:37:57 +0600

    jwt applied in admin get

--- index.js
+++ index.js
@@ -235,8 +235,8 @@ async function run() {
     });
 
     // get an admin
-    // app.get("/admin/:email", varifyJwt ,async (req, res) => {
-    app.get("/admin/:email" ,async (req, res) => {
+    app.get("/admin/:email", varifyJwt ,async (req, res) => {
+    // app.get("/admin/:email" ,async (req, res) => {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
       const isAdmin = user?.role === "admin";
