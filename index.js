6c81c4a2a1468a1b5a7723663226b540f367fb00
neyaznafiz <niiyazobug@gmail.com>
2022-08-11 23:41:04 +0600

    get all booking api for admin impliment

--- index.js
+++ index.js
@@ -174,6 +174,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/get-all-booking-info', async (req, res)=>{
+      const bookingInfoAdmin = await allBookingServiceCollection.find({}).toArray()
+      res.send(bookingInfoAdmin)
+    })
+
+    // booking infor for user, filter by email
     app.get("/booking-info/:email", varifyJwt, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.params.email;
