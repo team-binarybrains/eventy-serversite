ddebbd227bcc4f023e77515e2c84d6f135a9256f
neyaznafiz <neyaznafiz@outlook.com>
2023-01-25 15:38:07 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 // const { application } = require("express");
 var jwt = require("jsonwebtoken");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
